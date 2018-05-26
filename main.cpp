91a93c8cd0d45b90fc2328aa0b488f536573e6c2
Pangratie Andrei <andrpangr@gmail.com>
2018-05-27 00:25:46 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ int main (int argc, char const *argv[])
 		newWindow.focus();
 		glViewport(0, 0, w, h);
 		shader.setMatrix("projectionMatrix", Math::projection<float>(
-				55.0, newWindow.width / (float)newWindow.height, 0.1, 1000));
+				55.0, newWindow.width / (float)newWindow.height, 0.1, 10000));
 	});
 
 	shader.setMatrix("viewMatrix", Math::identity<4, float>());
@@ -61,38 +61,53 @@ int main (int argc, char const *argv[])
 	glCullFace(GL_BACK);
 	glClearColor(1, 1, 1, 1);
 
-	Mesh<NormalVertexType> myMesh;
 	Camera testCamera;
 
-	// Util::addCube(myMesh, 10);
-	Util::addSphere(myMesh, 6, 30, Math::Vec4f(0.44, 0.9, 0.1, 1));
-	DeprecatedVBOMeshDraw gMesh(myMesh);
+	Mesh<NormalVertexType> pot;
+	Mesh<NormalVertexType> tile1;
+	Mesh<NormalVertexType> tile2;
+
+	Util::addCube(pot, 3, Math::Vec4f(0.44, 0.9, 0.1, 1));
+	Util::addSquare(tile1, 45, Math::Vec4f(0.34, 0.56, 0.1, 1),
+			Math::rot4<float>(-90, World::west));
+	Util::addSquare(tile2, 45, Math::Vec4f(0.74, 0.23, 0.56, 1),
+			Math::rot4<float>(-90, World::west));
+	// Util::addSphere(myMesh, 6, 30, );
+
+	DeprecatedVBOMeshDraw gPot(pot);
+	DeprecatedVBOMeshDraw gTile1(tile1);
+	DeprecatedVBOMeshDraw gTile2(tile2);
 
 	while (newWindow.active) {
 		if (newWindow.handleInput()) {
 			if (newWindow.keyboard.getKeyState(newWindow.keyboard.ESC))
 				newWindow.requestClose();
 		}
+		float speed = 1.0f;
+		if (newWindow.keyboard.getKeyState(newWindow.keyboard.L_SHIFT))
+			speed = 10;
 		if (newWindow.keyboard.getStateNoCase('W'))
-			testCamera.moveForward();
+			testCamera.moveForward(speed);
 		if (newWindow.keyboard.getStateNoCase('S'))
-			testCamera.moveBackward();
+			testCamera.moveBackward(speed);
 		if (newWindow.keyboard.getStateNoCase('A'))
-			testCamera.moveLeft();
+			testCamera.moveLeft(speed);
 		if (newWindow.keyboard.getStateNoCase('D'))
-			testCamera.moveRight();
+			testCamera.moveRight(speed);
 		if (newWindow.keyboard.getStateNoCase('Q'))
-			testCamera.moveDown();
+			testCamera.moveDown(speed);
 		if (newWindow.keyboard.getStateNoCase('E'))
-			testCamera.moveUp();
+			testCamera.moveUp(speed);
 
-		if (newWindow.mouse.lmb)
+		if (newWindow.mouse.lmb) {
 			testCamera.rotate(
 				newWindow.mouse.x - newWindow.mouse.lastX,
 				newWindow.mouse.y - newWindow.mouse.lastY,
 				newWindow.width,
 				newWindow.height
 			);
+			newWindow.mouse.update();
+		}
 
 		shader.setMatrix("viewMatrix", testCamera.getTransform());
 		
@@ -103,7 +118,11 @@ int main (int argc, char const *argv[])
 			shader.setMatrix("worldMatrix", Math::translation<float>(
 					(i / 10) * 100 * World::north + (i % 10) * 100 * World::west));
 			FixedFunctionMeshDraw().draw(mesh, shader);
-			gMesh.draw(shader);
+			if ((i / 10) % 2 && (i % 10) % 2)
+				gTile1.draw(shader);
+			else
+				gTile2.draw(shader);
+			gPot.draw(shader);
 		}
 		// gMesh.draw(shader);
 		// gdMesh.draw(shader);
